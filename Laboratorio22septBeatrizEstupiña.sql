9bd8691d947d491d669b5caa98c8efe3d1c770f4
beaestupina <bea.ea.2001@outlook.com>
2025-09-22 17:00:45 +0200

    Add files via upload

---
+++ Laboratorio22septBeatrizEstupiña.sql
@@ -0,0 +1,8 @@
+SELECT*FROM likes;
+SELECT*FROM usuarios;
+SELECT*FROM seguidores;
+SELECT*FROM comentarios;
+SELECT*FROM videos;
+SELECT*FROM videos JOIN usuarios;
+SELECT*FROM videos JOIN comentarios;
+SELECT*FROM usuarios JOIN seguidores;
